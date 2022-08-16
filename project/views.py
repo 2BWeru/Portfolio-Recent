5f0416c7c87f50b8396244400c3aeeb54c8263c9
Wamaitha <wamaithaweru19@gmail.com>
2022-08-16 15:09:34 +0300

    update home function

--- project/views.py
+++ project/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        Fullname = request.POST['Fullname']
+        Phonenumber = request.POST['Phone']
+        Email = request.POST['Email']
+        Subject = request.POST['Subject']
+        Message = request.POST['Message']
 
-    return render(request,'index.html')
+        # send email
+        send_mail(
+            Subject,
+            "From: "+ Fullname + " \n\n "+ "Phone Number: "+ Phonenumber +" \n\n  "+"Subject: " + Subject + " \n\n\n" + Message + "\n\n"+"Thank You,"+"\n\n\n"+ "@ Expanse Products Ltd",#message
+            Email,# from 
+            [settings.EMAIL_HOST_USER,],#to email
+        )
+
+        return render(request,'home.html',{'Message':Message,'Email':Email,'Fullname':Fullname,'Phonenumber':Phonenumber,"Subject":Subject})
+    else:
+        return render(request,'index.html')
